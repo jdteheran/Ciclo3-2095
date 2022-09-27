79e32b7702e6d2282b771f357cf7a7dcf8c577d6
Juan Teheran <jdteheran@Juans-MacBook-Pro.local>
2022-09-27 06:56:27 -0500

    js

--- JS CRUD/assets/js/script.js
+++ JS CRUD/assets/js/script.js
@@ -40,15 +40,17 @@ btn_crear.addEventListener('click', (event) => {
     let correo = document.getElementById('correo')
     let pass = document.getElementById('contrasena')
 
+    let id = uuidv4()
+
     if (validacion()) {
         let template_registro = `<tr>
-                                    <th scope="row">${uuidv4()}</th>
+                                    <th scope="row">${id}</th>
                                     <td>${nombre.value} - ${apellido.value}</td>
                                     <td>${correo.value}</td>
                                     <td>
-                                        <button class="btn btn-success">Ver</button>
-                                        <button class="btn btn-warning">Editar</button>
-                                        <button class="btn btn-danger">Eliminar</button>
+                                        <button class="btn btn-success ver">Ver</button>
+                                        <button class="btn btn-warning editar">Editar</button>
+                                        <button class="btn btn-danger eliminar">Eliminar</button>
                                     </td>
                                 </tr>`
 
@@ -57,6 +59,7 @@ btn_crear.addEventListener('click', (event) => {
         cuerpo_table.innerHTML = cuerpo_table.innerHTML + template_registro
 
         let estudiante = {
+            id,
             nombre: nombre.value,
             apellido: apellido.value,
             celular: celular.value,
@@ -64,7 +67,15 @@ btn_crear.addEventListener('click', (event) => {
             pass: pass.value,
         }
 
-        window.localStorage.setItem('estudiantes', JSON.stringify(estudiante))
+        let vector_estudiantes = window.localStorage.getItem('estudiantes')
+        let estudiantes = []
+
+        if (vector_estudiantes != null) {
+            estudiantes = JSON.parse(vector_estudiantes)
+        }
+
+        estudiantes.push(estudiante)
+        window.localStorage.setItem('estudiantes', JSON.stringify(estudiantes))
 
         ver_lista()
 
@@ -84,19 +95,53 @@ function validacion() {
     let correo = document.getElementById('correo').value
     let pass = document.getElementById('contrasena').value
 
-    if (nombre.length < 3) {alert('El campo nombre es obligatorio, minimo 3 caracteres'); return false}
-    if (apellido.length < 3) {alert('El campo apellido es obligatorio, minimo 3 caracteres'); return false}
-    if (celular.length < 10) {alert('El campo celular es obligatorio, minimo 10 numeros'); return false}
+    if (nombre.length < 3) { alert('El campo nombre es obligatorio, minimo 3 caracteres'); return false }
+    if (apellido.length < 3) { alert('El campo apellido es obligatorio, minimo 3 caracteres'); return false }
+    if (celular.length < 10) { alert('El campo celular es obligatorio, minimo 10 numeros'); return false }
 
-    if (!/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i.test(correo)) {alert('El correo no cumple con los requisitos'); return false}
+    if (!/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i.test(correo)) { alert('El correo no cumple con los requisitos'); return false }
 
-    if (!/^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/i.test(pass)) {alert('La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula, al menos una mayúscula y al menos un caracter no alfanumérico.'); return false}
+    if (!/^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/i.test(pass)) { alert('La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula, al menos una mayúscula y al menos un caracter no alfanumérico.'); return false }
 
     return true
 }
 
 function uuidv4() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
-  }
+}
+
+let estudiantes = window.localStorage.getItem('estudiantes')
+
+if (estudiantes != null) {
+    estudiantes = JSON.parse(estudiantes)
+
+    estudiantes.forEach(estudiante => {
+        let template_html = `<tr>
+                                <th scope="row">${estudiante.id}</th>
+                                <td>${estudiante.nombre} - ${estudiante.apellido}</td>
+                                <td>${estudiante.correo}</td>
+                                <td>
+                                    <button class="btn btn-success ver">Ver</button>
+                                    <button class="btn btn-warning editar">Editar</button>
+                                    <button class="btn btn-danger eliminar">Eliminar</button>
+                                </td>
+                            </tr>`
+
+        let cuerpo_table = document.querySelector('tbody')
+
+        cuerpo_table.innerHTML = cuerpo_table.innerHTML + template_html
+    });
+
+    let btns_ver = document.querySelectorAll('.ver')
+
+    btns_ver.forEach(btn => {
+        
+        btn.addEventListener('click', () => {
+            let id_estudiante = btn.parentElement.parentElement.firstElementChild.textContent
+            console.log(btn.parentElement.parentElement.childNodes)
+        })
+
+    });
+}
