fcf7d06c1147274dc28266b927d83861844685eb
Juan Teheran <jdteheran@Juans-MacBook-Pro.local>
2022-10-01 09:55:34 -0500

    render template flask

--- Backend/app.py
+++ Backend/app.py
@@ -1,21 +1,8 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 import json
 app = Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
-def hola():
-    diccionario = {
-        'nombre': 'Juan',
-        'apellido': 'Teheran',
-        'correo': 'jdteheran@uninorte.edu.co'
-        }
-    return jsonify(diccionario)
-
-@app.route('/login', methods=['POST'])
-def login():
-    variable = json.loads(request.data.decode("utf-8"))
-    print(variable)
-    print(variable['user'])
-    print(variable['password'])
-    return 'login exitoso'
-
+@app.route('/', methods=['GET'])
+def inicio():
+    return render_template('index.html', hola='Uninorte')
+    
