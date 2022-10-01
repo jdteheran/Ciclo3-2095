40e18a0052621f7020c4e69ec69a7d77f6ac4cfc
Juan Teheran <jdteheran@Juans-MacBook-Pro.local>
2022-10-01 09:33:57 -0500

    rutas flask

--- Backend/app.py
+++ Backend/app.py
@@ -1,25 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import json
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def hola():
-    return 'Hola mundo'
+    diccionario = {
+        'nombre': 'Juan',
+        'apellido': 'Teheran',
+        'correo': 'jdteheran@uninorte.edu.co'
+        }
+    return jsonify(diccionario)
 
-@app.route('/contacto')
-def contacto():
-    return 'pantalla de contacto'
+@app.route('/login', methods=['POST'])
+def login():
+    variable = json.loads(request.data.decode("utf-8"))
+    print(variable)
+    print(variable['user'])
+    print(variable['password'])
+    return 'login exitoso'
 
-
-@app.route('/perfil')
-def pefil():
-    return 'pantalla de perfil'
-
-@app.route('/api/<string:nombre>')
-def saludar(nombre):
-    return 'mandamos un saludo a:' + nombre
-
-@app.route('/api2')
-@app.route('/api2/<string:nombre>')
-@app.route('/api2/<string:nombre>/<string:apellido>')
-def credencial(nombre='vacio', apellido='vacio'):
-    return 'el apellido de {} es {}'.format(nombre, apellido)
