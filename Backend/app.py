59d6eee62d15736fd5850d7c85f83bb6c68bc385
Juan Teheran <jdteheran@Juans-MacBook-Pro.local>
2022-10-01 08:39:37 -0500

    flask peticiones

--- Backend/app.py
+++ Backend/app.py
@@ -1,9 +1,25 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def hola():
     return 'Hola mundo'
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+@app.route('/contacto')
+def contacto():
+    return 'pantalla de contacto'
+
+
+@app.route('/perfil')
+def pefil():
+    return 'pantalla de perfil'
+
+@app.route('/api/<string:nombre>')
+def saludar(nombre):
+    return 'mandamos un saludo a:' + nombre
+
+@app.route('/api2')
+@app.route('/api2/<string:nombre>')
+@app.route('/api2/<string:nombre>/<string:apellido>')
+def credencial(nombre='vacio', apellido='vacio'):
+    return 'el apellido de {} es {}'.format(nombre, apellido)
